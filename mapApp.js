fa9294440c874f3244aadb17ca076af5e9ce723f
ajwwlswotjd <ajwwlswotjd@naver.com>
2020-07-04 03:05:43 +0900

    json 완성

--- mapApp.js
+++ mapApp.js
@@ -1,15 +1,17 @@
 const log = console.log;
-const times = 20;
+const times = 40;
+let mode = 1;
 class App {
 	constructor(list){
 		this.cntArr = [0,0,0,0];
 		this.canvas = document.querySelector("#canvas");
 		this.ctx = this.canvas.getContext("2d");
+		this.drag = false;
 		this.arr = [];
-		for(let i = 0; i < list.length; i++){
-		// for(let i = 0; i < 50; i++){
+		for(let i = 0; i < 100; i++){
+		// for(let i = 0; i < 49; i++){
 			let arrTemp = [];
-			for(let j = 0; j < list[i].length; j++){
+			for(let j = 0; j < 125; j++){
 			// for(let j = 0; j < 50; j++){
 				let locate = new Locate(j,i,this.ctx,list[i][j]);
 				this.cntArr[list[i][j]]++;
@@ -17,16 +19,40 @@ class App {
 				locate.draw();
 			}
 			this.arr.push(arrTemp);
-			document.querySelector(".empty > span").innerHTML = this.cntArr[0];
-			document.querySelector(".street > span").innerHTML = this.cntArr[1];
-			document.querySelector(".sleep > span").innerHTML = this.cntArr[2];
-			document.querySelector(".festival > span").innerHTML = this.cntArr[3];
 		}
+		document.querySelector(".empty > span").innerHTML = this.cntArr[0];
+		document.querySelector(".street > span").innerHTML = this.cntArr[1];
+		document.querySelector(".sleep > span").innerHTML = this.cntArr[2];
+		document.querySelector(".festival > span").innerHTML = this.cntArr[3];
 
-		this.canvas.addEventListener("click",(e)=>{
+		window.addEventListener("mousedown",(e)=>{
+			this.drag = true;
+		});
+
+		window.addEventListener("mouseup",(e)=>{
+			this.drag = false;
+		});
+
+		document.querySelector("#mode").addEventListener("input",(e)=>{
+			mode = e.target.value*1;
+		});
+
+		this.canvas.addEventListener("mousemove",(e)=>{
+			if(!this.drag) return;
 			let x = Math.floor(e.offsetX/times);
 			let y = Math.floor(e.offsetY/times);
 			let locate = this.arr[y][x];
+			this.cntArr[locate.value]--;
+			locate.dragFill();
+			this.cntArr[locate.value]++;
+		});
+
+		this.canvas.addEventListener("click",(e)=>{
+			if(this.drag) return;
+			let x = Math.floor(e.offsetX/times);
+			let y = Math.floor(e.offsetY/times);
+			let locate = this.arr[y][x];
+			log(locate);
 			this.cntArr[locate.value]--;
 			locate.click();
 			this.cntArr[locate.value]++;
@@ -38,14 +64,31 @@ class App {
 
 		document.querySelector("#btn").addEventListener("click",(e)=>{
 			let arr = [];
-			for(let i = 0; i < list.length; i++){
+			for(let i = 0; i < this.arr.length; i++){
 				let arrTemp = [];
-				for(let j = 0; j < list[0].length; j++){
+				for(let j = 0; j < this.arr[0].length; j++){
 					arrTemp.push(this.arr[i][j].value);
 				}
 				arr.push(arrTemp);
 			}
-			// log(JSON.stringify(arr,null,0));
+			
+			let fesArr = [];
+			let sleepArr = [];
+			log(this.arr);
+			for(let i = 0; i < this.arr.length; i++){
+				for(let j = 0; j < this.arr[0].length; j++){
+					let locate = this.arr[i][j];
+					let value = locate.value;
+					if(value == 2){ // 숙박임
+						sleepArr.push([j,i]);
+					} else if(value == 3){ // 축제임
+						fesArr.push([j,i]);
+					}	
+				}
+			}
+			log(sleepArr);
+			log(fesArr);
+
 			down(arr);
 		});
 
@@ -61,7 +104,13 @@ class Locate {
 		this.y = y;
 		this.ctx = ctx;
 		this.value = value;
+		this.drag = false;
 		this.colorArr = ["#fff","#0d7811","#ff7777","#c13bff"];
+	}
+
+	dragFill(){
+		this.value = mode;
+		this.draw();
 	}
 	
 	click(){
